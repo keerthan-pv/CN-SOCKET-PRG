f11c3386a6cf8ca95d2b75af8f879812f30f455d
Keerthan pv <keerthanpv2004@gmail.com>
2025-04-30 00:09:47 +0530

    Add files via upload

---
+++ client1.py
@@ -0,0 +1,31 @@
+import socket
+import ssl
+
+USE_PROXY_SERVER = False
+SERVER_ADDRESS = ('127.0.0.1', 8443) if USE_PROXY_SERVER else ('127.0.0.1', 8888)
+
+if USE_PROXY_SERVER:
+    context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
+
+    with socket.create_connection(SERVER_ADDRESS) as sock:
+        with context.wrap_socket(sock, server_hostname='localhost') as ssock:
+            password = input("Enter password: ")
+            ssock.sendall(password.encode()) 
+            auth_response = ssock.recv(1024)
+            decoded_response = auth_response.decode()
+
+            if decoded_response.lower().startswith("access denied"):
+                print("Incorrect password. You are blocked.")
+            else:
+                print("Connection established.")
+                message = input("Enter your message: ")
+                ssock.sendall(message.encode())
+                response = ssock.recv(1024)
+                print("Server response:", response.decode())
+else:
+    with socket.create_connection(SERVER_ADDRESS) as sock:
+        sock.sendall(b"Hello from Plain Client1!")
+        response = sock.recv(1024)
+        print("Server response:", response.decode())
